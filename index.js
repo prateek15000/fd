22cbaad4aa90dbb76b331dda122ac505f0cedd05
Prateek Mittal <134774647+prateek15000@users.noreply.github.com>
2024-09-22 18:01:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 //console.log("prateek mittal");
+//console.log("prateek mittal");
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+ const prateek={
+    "name" : "prateek",
+    "age" : 20,
+    "money" : "infinite",
+    "joy": 15,
+    "toy": "TransformStream",
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -17,6 +25,9 @@ app.get('/ch',(req,res)=>{
 
 app.get('/youtube',(req,res)=>{
     res.send("hey this is youtube");
+})
+app.get('/github',(req,res)=>{
+    res.json(prateek);
 })
 
 app.listen(process.env.PORT, () => {
